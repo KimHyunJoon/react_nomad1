b206ee677fe375e4619873c284698dc8aaa3b7f4
KimHyunJoon <hg224142@gmail.com>
2023-01-03 16:30:07 +0900

    hello2

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,7 +4,7 @@ import Coin from "./routes/Coin_type";
 
 function Router() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path="/:coinId">
           <Coin />
